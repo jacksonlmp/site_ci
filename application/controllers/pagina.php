e26ead49c5f4e0eff2bc46d164992db5958cf2b6
jacksonlmp <jacksonlmp17.jl@gmail.com>
2021-08-24 09:08:52 -0300

    CodeIgniter

---
+++ application/controllers/pagina.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exist('NO Direct acess allower');
+
+class Pagina extends CI_Controller{
+
+    function __contruct(){
+        parent::__contruct();
+    }
+
+    public function index(){
+        echo 'alright';
+    }
+
+}
